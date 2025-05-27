5a42b5748bc5a9af39f0f089a46afd8515a4736a
Ekta-pawar <ektapawar0001@gmail.com>
2025-05-27 12:33:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 function highlight() {
     //Write your code here
-
+const doc=
+	document.querySelectorAll('strong');
+	doc.forEach((word)=>{
+		word.style.color='rgb(0, 128, 0)';
+	});
 
 }
 
 
 function return_normal() {
     //Write your code here
+const doc=
+	document.querySelectorAll('strong');
+	doc.forEach((word)=>{
+		word.style.color='rgb(0, 0, 0)';
+	});
 
     
 }
